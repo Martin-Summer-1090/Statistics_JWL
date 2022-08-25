c0561470a1b4a872f2cfeb51a386dc2c9f25b94b
your name <your e-mailadress>
2022-08-25 15:17:20 +0200

    first draft or Introduction for discussion in JWL JF 25.8.2022

---
+++ notebooks/extreme_poverty_world_1981_2019.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+
+demo_data <- read.csv("../data/extreme_poverty/share-of-population-in-extreme-poverty.csv")
+
+names(demo_data) <- c("Country", "Code", "Year", "Share")
+
+pl_dat <- with(demo_data, demo_data[Country %in% c("World", "China") & Year >= 2000, ])
+
+p <- ggplot(pl_dat, aes(x = Year, y = Share, color = Country)) +
+     geom_point() +
+     geom_line() +
+     xlab("")
+
+p
